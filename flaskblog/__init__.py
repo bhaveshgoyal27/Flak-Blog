a817c46e4021d096916d6207ec67d17ed7ae1cf0
bhaveshgoyal27 <27goyalbhavesh@gmail.com>
2021-11-05 23:34:23 +0530

    code

---
+++ flaskblog/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ce24f963ea49a7a006767cd4882977e2fc602dd94e61ced2e07760a6ecb89678'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' #name of the function which redirects to login.html
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
+from flaskblog import routes
